cb1ca24d0f8c81f56afa0769f26e31ee59af298a
ramiro-l <ramideporte@gmail.com>
2024-07-22 22:45:42 -0300

    feat: Add config.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,15 @@
 import { defineConfig } from "astro/config";
-
 import tailwind from "@astrojs/tailwind";
+import react from "@astrojs/react";
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [tailwind()],
+    integrations: [
+        tailwind({
+            applyBaseStyles: false,
+        }),
+        react(),
+        icon(),
+    ],
 });
